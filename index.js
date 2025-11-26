fc7ad847e271b7a8bdf63e372901795fd10e1306
Ronald <guerrerotoloza.ro@gmail.com>
2025-11-25 21:06:43 -0500

    [+] Creacion de los modulos y creacion de .gitignore para omitir la subida de esos modulos

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("project-jewelry");
+const express = require("express");         // Importacion
+const app = express();                      // Invocando core Express
+const PORT = 3000;                          
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`)
+});
