10c0a39c3f9855a4e6a0e1f20f9aaa4367228d49
felipe cardenas <felipecas097@gmail.com>
2025-12-01 18:54:26 -0500

    index.js: Middleware Parse JSON [+] user.route.js: Obtiene los datos de la peticion

--- src/index.js
+++ src/index.js
@@ -1,23 +1,26 @@
-import express from "express";
+import express from 'express';  //importacion
+import dbConnection from './config/mongo.config.js';
+import userRoute from './routes/user.route.js';
+import productsRoute from './routes/products.route.js';
 
-import dbConnection from './config/mongo.config.js'
-import userRoute from './routes/user.route.js'
-import productRoute from './routes/products.route.js'
-
-const app = express();                      // Invocando core Express
-const PORT = 3000;   
+const app = express(); //invocando core
+const PORT = 3000;
 
 dbConnection();
 
-// Middleware Express
+app.get("/health", (req, res) => {
+  res.json({
+    weight: "180 lb",
+    height: "6.0 ft",
+  });
+});
+// Middlewares Express
+app.use (express.json() );
 
-// app.use('/api/v1/user', require('./routes/user.route.js'/*,'./routes/products.route.js' => no se puede meter 2 en una sola*/))
-// // app.use('/api/v1', require('./routes/products.route.js'))  => preferible poner la direccion directamente y no en el archivo
-// app.use('/api/v1/product', require('./routes/products.route.js'))
-app.use('/api/v1/user', userRoute);
-app.use('/api/v1/product', productRoute);
+app.use("/api/v1/users", userRoute);
+app.use("/api/v1/products", productsRoute);
 
-
+//Lanzando el servidor a la web
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}/api/v1/user`)
+  console.log(`Server running on http://localhost:${PORT}`);
 });
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -2,28 +2,32 @@ import express from 'express'
 
 const router = express.Router();
 
-router.get( '/', ( req, res ) => {
-    res.json({ msg: 'Obtiene todos los usuarios' });
-} );
+// router.get( '/', ( req, res ) => {
+//     res.json({ msg: 'Obtiene todos los usuarios' });
+// } );
 
-router.post( '/', ( req, res ) => {
-    res.json({ msg: 'Crear un usuario' });
-} );
+// router.post( '/', ( req, res ) => {
+//     res.json({ msg: 'Crear un usuario' });
+// } );
 
-router.put( '/', ( req, res ) => {
-    res.json({ msg: 'Actualiza todos las propiedades del usuario' });
-} );
+// router.put( '/', ( req, res ) => {
+//     res.json({ msg: 'Actualiza todos las propiedades del usuario' });
+// } );
 
-router.patch( '/', ( req, res ) => {
-    res.json({ msg: 'Actualiza parcialmente 1 o todas las propiedades del usuario' });
-} );
+// router.patch( '/', ( req, res ) => {
+//     res.json({ msg: 'Actualiza parcialmente 1 o todas las propiedades del usuario' });
+// } );
 
-router.delete( '/', ( req, res ) => {
-    res.json({ msg: 'Elimina un usuario' });
-} );
+// router.delete( '/', ( req, res ) => {
+//     res.json({ msg: 'Elimina un usuario' });
+// } );
 
-// router.get('/alo', (req, res) => {
-//     res.send('<h1>alo</h1>')
-// });
+router.post( '/',( req, res ) => {
+    const data = req.body;
+
+    console.log( data);
+    res.json({ msg:'create users', data
+    })
+})
 
 export default router;
