f35e9a1948483bef2431a0a6d481932ec4444ad4
felipe cardenas <felipecas097@gmail.com>
2025-12-02 18:23:15 -0500

    [$] User.model.js: singular del nombre de la collection. [+] user.controller.js: Manejar la exception que produce registerUser()

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,21 +1,33 @@
 // controlador: se debe encargar de recibir las peticiones y responder a ellas
 import { registerUser } from "../services/user.services.js";
 const createUser =  async ( req, res ) => {
-    const data = req.body;
 
-    console.log( data);
+    // Se controla la excepcion que ocurre en el paso 2
+    try{
+        //Paso 1: extraer el cuerpo de la peticion
+        const data = req.body;    
 
-    //Registrar los datos usando el userModel
-   const dataRegistered = await registerUser ( data );   //Registrar los datos en la base de datos
-
-    // Responder al cliente
-    res.json({ msg:'create users',
-         //data: data,             // Forma tradicional
-         dataRegistered            // ECMAScript 2015 
-    });
-}; 
-
-
+        //Mostrar en la consola el cuerpo de la peticion
+        console.log( data);
+    
+        //Paso 2: Registrar los datos usando el userModel
+       const dataRegistered = await registerUser ( data );   //Registrar los datos en la base de datos
+    
+        //Paso 3: Responder al cliente
+        res.json({ 
+            msg:'create users',
+             //data: data,             // Forma tradicional
+             dataRegistered            // ECMAScript 2015 
+        });
+    }   
+    catch (error) {
+        console.error( error );
+        res.json({
+            msg: 'Error: No se pudo crear el usuario'
+        });
+    }
+} 
+    
 export {
     createUser
-};
+}
